a00908626b69b7ebb9f140bd4ce1bb6f5c3de475
ManishSubedi <100317658+Manish-Subedi@users.noreply.github.com>
2024-08-24 15:21:22 +0300

    Update config.h

--- robot_controller/config.h
+++ robot_controller/config.h
@@ -1,41 +1,31 @@
 // <<< Use Configuration Wizard in Context Menu >>>
-
-//<h>系统配置
-	//<e>调试信息
+//<h>
+	//<e>
 	#define DEBUG_ENABLE					1
 	//</e>
-
 //</h>
 #define DEBUG_CHAR  0
-
-//<h>小车控制配置
-	//<e>陀螺仪
+//<h>
+	//<e>
 	#define MPU6xxx_ENABLE					1
-		//<c>使用板载IMU
+		//<c>
 		#define  USE_BOARD_IMU  			1
 		//</c>
 
-		//<c>启用Z轴自旋
+		//<c>
 		#define  AXLE_Z_RESTRAIN  			0
 		//</c>
-	//</e>
-	
-	//<e>使用编码器
+	//</e>	
+	//<e>
 	#define ENCODER_ENABLE					1
-
-		//<c>编码器方向 1: 正向，0: 反向
+		//<c>
 		#define ENCODER_DIRECTION			1
 		//</c>
 	//</e>
-
 //</h>
-
-
-//<e>OLED显示控制
-//<i>选择是否开启OLED显示
+//<e>OLED
+//<i>
 #define  OLED_DISPLAY_ENABLE		0
-
 //</e>
-
 // <<< end of configuration section >>>
 
